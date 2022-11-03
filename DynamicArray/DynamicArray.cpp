aadb658e68b74ff0328da36bd1870bd40c60b2f7
DanilBakhtin <72213151+DanilBakhtin@users.noreply.github.com>
2022-11-03 14:40:21 +0300

    Add files via upload

---
+++ DynamicArray/DynamicArray.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include "MyArray.h"
+using namespace std;
+
+
+int main()
+{	
+	setlocale(LC_ALL, "Russian");
+	
+	MyArray<int> a;
+
+	for (int i = 0; i < 10; ++i)
+		a.insert(i + 1);
+
+	for (int i = 0; i < a.size(); ++i)
+		a[i] *= 2;
+
+	cout << "Array A:\n";
+	for (auto it = a.iterator(); it.hasNext(); it.next())
+		std::cout << it.get() << std::endl;
+
+	cout << "Reverse Array A:\n";
+	for (auto it = a.reverseIterator(); it.hasNext(); it.next())
+		std::cout << it.get() << std::endl;
+		
+}
+
